156da9852fe608c5399c317868d7dc3ef8a61251
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2024-05-03 17:11:50 +1000

    Initial commit

---
+++ src/main/java/ind/ivankuznetsov/sber/task/first/Main.java
@@ -0,0 +1,7 @@
+package ind.ivankuznetsov;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/ind/ivankuznetsov/sber/task/first/model/City.java
@@ -0,0 +1,2 @@
+package ind.ivankuznetsov.sber.task.first.model;public record City() {
+}
